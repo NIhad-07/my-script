f4bdbf4ffab1f74b70996bde5c70c86e1a4eb799
Nihad-07 <nihadpulikkapparambil@gmail.com>
2024-08-07 12:09:44 +0530

    file added

---
+++ filesindirectory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+read -p "Enter directory name: " directory
+
+
+if [ -d "$directory" ]; then
+  file_count=$(find "$directory" -type f | wc -l)
+  echo "Number of files in $directory: $file_count"
+else
+  echo "Directory $directory does not exist"
+fi
+
